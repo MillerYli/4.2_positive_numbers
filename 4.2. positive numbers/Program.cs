9c931f3cbfb1a1889b6fe53c9d8bc67a03c11a96
Yulia Miller <miller82.98@mail.ru>
2022-12-14 23:43:55 -0800

    Добавьте файлы проекта.

---
+++ 4.2. positive numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace mynamespase
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int sum = 0;
+
+            while(true)
+            {
+                Console.WriteLine("Введите число: ");
+                var number = Convert.ToInt32(Console.ReadLine());
+
+                if(number < 0)
+                {
+                    continue;
+                }
+                else if(number == 0) 
+                {
+                    break;
+                }
+                else
+                {
+                    sum += number;
+                }
+                
+            }
+            Console.WriteLine("Сумма введеных положительных чисел: {0}", sum);
+        }
+    }
+}
